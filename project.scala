08f5a15cc44ad07c2fdbdb943d8c77188197ce0f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-03-21 21:09:46 +0100

    Update project publishing URL and VCS information

--- project.scala
+++ project.scala
@@ -4,8 +4,8 @@
 
 //> using publish.name "scala-cli-md-spec"
 //> using publish.organization "com.kubuszok"
-//> using publish.url "https://github.com/MateuszKubuszok/scala-cli-md-spec"
-//> using publish.vcs "github:MateuszKubuszok/scala-cli-md-spec"
+//> using publish.url "https://github.com/kubuszok/scala-cli-md-spec"
+//> using publish.vcs "github:kubuszok/scala-cli-md-spec"
 //> using publish.license "Apache-2.0"
 //> using publish.repository "central"
 //> using publish.developer "MateuszKubuszok|Mateusz Kubuszok|https://github.com/MateuszKubuszok"
